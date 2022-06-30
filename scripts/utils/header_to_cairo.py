1eeb27c0317f2098ffb66c58342fc02bea388284
Abdelhamid Bakhta <45264458+abdelhamidbakhta@users.noreply.github.com>
2022-06-30 08:52:06 +0200

    🎨  fix format (#37)

--- scripts/utils/header_to_cairo.py
+++ scripts/utils/header_to_cairo.py
@@ -1,14 +1,20 @@
 import json
 
+
 def hash_to_Uint256_cairo(hash_hex_str):
     low = hash_hex_str[32:]
     high = hash_hex_str[:32]
     return f"Uint256(low=0x{low}, high=0x{high})"
-    
+
+
 def header_to_cairo(block_json):
-    prev_block = block_json["previousblockhash"] if "previousblockhash" in block_json else "0"*64
-    hash = block_json["hash"] if "hash" in block_json else "0"*64
-    merkle_root = block_json["merkleroot"] if "merkleroot" in block_json else "0"*64
+    prev_block = (
+        block_json["previousblockhash"]
+        if "previousblockhash" in block_json
+        else "0" * 64
+    )
+    hash = block_json["hash"] if "hash" in block_json else "0" * 64
+    merkle_root = block_json["merkleroot"] if "merkleroot" in block_json else "0" * 64
     res = f"""\
 BlockHeader(
     version= {block_json['version']}, 
@@ -25,15 +31,18 @@ BlockHeader(
 
 def main():
     import sys
-    input_name = sys.argv[1] 
 
-    if input_name is None: return
+    input_name = sys.argv[1]
+
+    if input_name is None:
+        return
 
     with open(input_name) as block_file:
         block = json.load(block_file)
-    
+
     res = header_to_cairo(block)
     print(f"let header: BlockHeader = {res}")
+
 
 if __name__ == "__main__":
     main()
